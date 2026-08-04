4771d75f59947c5bad53cd1b13e53680493a35e3
Joao <joaopaulocardosokls@gmail.com>
2026-08-04 21:57:55 +0100

    Bug Input Fixed && See all tasks

--- main.c
+++ main.c
@@ -9,7 +9,8 @@ struct tasks {
 
 int main() {
   char priority[100];
-  char option;
+  int option;
+  char line[256];
 
   struct tasks t;
 
@@ -23,34 +24,42 @@ int main() {
 
   printf("-- All tasks -- \n");
   printf("1 - Create new task \n");
+  printf("2 - View all tasks \n");
 
-  while (1) {
-    scanf(" %c", &option);
+  scanf(" %d", &option);
 
-    if (option == '1') {
-      printf("-- New task -- \n");
-      printf("Task Name : \n");
-      fgets(t.name, sizeof(t.name), stdin);
-      getchar();
-
-      printf("Task Description:  \n");
-      fgets(t.description, sizeof(t.description), stdin);
-      getchar();
-
-      printf("Task Priority:  \n");
-      printf("1 - Urgent / 2 - Medium / 3 - Not Important \n");
-      fgets(priority, sizeof(priority), stdin);
-
-      getchar();
-
-      fprintf(file, "Name: %sDescription: %sPriority: %s\n\n", t.name,
-              t.description, priority);
-
-      break;
-    }
+  int c;
+  while ((c = getchar()) != '\n' && c != EOF) {
   }
 
+  if (option == 1) {
+    printf("-- New task -- \n");
+    printf("Task Name : \n");
+    fgets(t.name, sizeof(t.name), stdin);
+
+    printf("Task Description:  \n");
+    fgets(t.description, sizeof(t.description), stdin);
+
+    printf("Task Priority:  \n");
+    printf("1 - Urgent / 2 - Medium / 3 - Not Important \n");
+    fgets(priority, sizeof(priority), stdin);
+
+    fprintf(file, "Name: %s\n Description: %s\n Priority: %s\n", t.name,
+            t.description, priority);
+    fclose(file);
+  }
+
+  if (option == 2) {
+    FILE *f;
+    f = fopen("task.txt", "r");
+
+    if (f == NULL) {
+      printf("Error: Could Not Open File");
+    };
+
+    while (fgets(line, sizeof(line), f) != NULL) {
+      printf("%s", line);
+    }
+  }
   return 0;
 }
-
-// i need to find a way to store the tasks in the cli
